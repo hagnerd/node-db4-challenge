2060486c1ec9eaa13ded0c1579cd957dd22f6c24
Matt Hagner <web@matthagner.com>
2019-08-31 09:15:12 -0500

    Add seeds and update migrations

--- migrations/20190829194939_recipes_ingredients.js
+++ migrations/20190829194939_recipes_ingredients.js
@@ -2,7 +2,7 @@
  * Obj {
  *   recipe_id: foreign_key(@recipes)!
  *   ingredient_id: foreign_key(@ingredients)!
- *   unit_of_measure: cup | teaspoon | tablespoon | oz
+ *   unit_of_measure: cup | teaspoon | tablespoon | oz | lbs
  *   quantity: float
  * }
  */
@@ -14,18 +14,24 @@ exports.up = function(knex) {
       .unsigned()
       .notNullable()
       .references("id")
-      .inTable("recipes");
+      .inTable("recipes")
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
 
     tbl
       .integer("ingredient_id")
       .unsigned()
       .notNullable()
       .references("id")
-      .inTable("ingredient");
+      .inTable("ingredients")
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
 
+
+    // this field is nullable because of ingredients such as:
+    // hamburger buns, or eggs, or onions
     tbl
-      .enum("unit_of_measure", ["cup", "teaspoon", "tablespoon", "oz"])
-      .notNullable();
+      .enum("unit_of_measure", ["cup", "teaspoon", "tablespoon", "oz", "lbs"]);
 
     tbl.float("quantity").notNullable();
   });
